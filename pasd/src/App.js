f739e2f692b5007167d05f672f787343795adcc3
Safi100 <safinafi12@gmail.com>
2025-01-27 01:36:28 +0200

    Create footer

--- pasd/src/App.js
+++ pasd/src/App.js
@@ -6,6 +6,7 @@ import Navbar2 from './components/Navbar2';
 import Buildings from './Buildings';
 import BuildingDetails from './BuildingDetails';
 import Main from "./Main";
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -22,11 +23,9 @@ function App() {
               <Route path="/Cities" element={"cities element"} />
             </Routes>
           </div>
-          <footer className="h">
-            <a href="a">Contact us | </a>
-            <a href="a">Privacy Policy</a>
-          </footer>
+          
         </div>
+        <Footer />
       </Router>
     </>
   );
---
+++ pasd/src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LanguageIcon from '@mui/icons-material/Language';
+import '../css/footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer_container'>
+        <div className="wrapper">
+            <div className="footer_row">
+                <a href='/' className='logo'><img src="https://www.alquds.edu/wp-content/uploads/2022/02/black-logo.png" alt="AQU LOGO" /></a>
+                <div>
+                    <h5>Address</h5>
+                    <p className='mb-0'>University of Al-Quds</p>
+                    <p className='mb-0'>Palestine, Jerusalem</p>
+                </div>
+                <div>
+                    <h5>Contact</h5>
+                    <p className='mb-0'>Phone: (+970) 2 2790606</p>
+                    <p className='mb-0'>Email: pr@alquds.edu</p>
+                </div>
+            </div>
+            <hr />
+            <div className='footer_bottom'>
+                <p>© {new Date().getFullYear()} All copyrights reserved for Al-Quds University.</p>
+                <p className='social_links'>
+                    <a href='https://www.alquds.edu/' target='_blank' rel='noopener noreferrer'><LanguageIcon /></a>
+                    <a href='https://ps.linkedin.com/school/alquds-university/' target='_blank' rel='noopener noreferrer'><LinkedInIcon /></a>
+                    <a href='https://www.instagram.com/alquds.university' target='_blank' rel='noopener noreferrer'><InstagramIcon /></a>
+                    <a href='https://www.youtube.com/@AlQudsUniv' target='_blank' rel='noopener noreferrer'><YouTubeIcon /></a>
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
