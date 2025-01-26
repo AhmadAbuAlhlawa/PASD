173bb6e8ae21da1f30d369153f359afb412d1fb2
Safi100 <safinafi12@gmail.com>
2025-01-27 01:36:55 +0200

    update main page bg

--- pasd/src/Main.jsx
+++ pasd/src/Main.jsx
@@ -1,9 +1,9 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import './css/Main.css';
 import Searchb from './components/Searchb';
 import About from './components/About';
-const Main = () => {
 
+const Main = () => {
   const [backgroundImage, setBackgroundImage] = useState('');
 
   const [images, setImages] = useState([
@@ -13,24 +13,35 @@ const Main = () => {
     'http://localhost:3000/imge/img_4.jpg',
     'http://localhost:3000/imge/img_5.jpg',
     'http://localhost:3000/imge/img_6.jpg',
-  ])
+  ]);
 
-  // Set a random background on component mount
+  // Change background image every 10 seconds
   useEffect(() => {
-    const randomImage = images[Math.floor(Math.random() * images.length)];
-    setBackgroundImage(randomImage);
-  }, []); // Empty dependency array ensures it runs only once on mount
+    const updateBackgroundImage = () => {
+      const randomImage = images[Math.floor(Math.random() * images.length)];
+      setBackgroundImage(randomImage);
+    };
+
+    // Set an initial background image
+    updateBackgroundImage();
+
+    // Set up interval for updating the background image
+    const intervalId = setInterval(updateBackgroundImage, 15000); // 15 seconds
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [images]);
 
   return (
     <>
-    <div
-     className="main-container"
-     style={{
-       backgroundImage: `linear-gradient(to bottom right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${backgroundImage})`
-    }}
-    >
-      <Searchb />
-    </div>
+      <div
+        className="main-container"
+        style={{
+          backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(${backgroundImage})`,
+        }}
+      >
+        <Searchb />
+      </div>
       <About />
     </>
   );
