7a8701d97a4bc566bbbfb36348e63fe5f9c10e4b
Safi100 <safinafi12@gmail.com>
2025-01-27 22:42:23 +0200

    fix class name

--- pasd/src/components/Navbar2.jsx
+++ pasd/src/components/Navbar2.jsx
@@ -6,37 +6,37 @@ function Navbar2() {
   const location = useLocation();
 
   return (
-<nav class="navbar2 navbar navbar-expand-lg sticky-top">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/">Home</a>
-        </li>
-        {location.pathname === "/" && (
-          <li className="nav-item">
-            <a className="nav-link" href="/#about_pasd">About PASD</a>
-          </li>
-        )}
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Archive</a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/Cities">Cities</a></li>
-            <li><a class="dropdown-item" href="/Buildings">Buildings</a></li>
-            <li><a class="dropdown-item" href="/Architects">Architects</a></li>
+    <nav className="navbar2 navbar navbar-expand-lg sticky-top">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="/">Home</a>
+            </li>
+            {location.pathname === "/" && (
+              <li className="nav-item">
+                <a className="nav-link" href="/#about_pasd">About PASD</a>
+              </li>
+            )}
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Archive</a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="/Cities">Cities</a></li>
+                <li><a className="dropdown-item" href="/Buildings">Buildings</a></li>
+                <li><a className="dropdown-item" href="/Architects">Architects</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/support-us">Support Us</a>
+            </li>
           </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/support-us">Support Us</a>
-        </li>
-      </ul>
-    </div>
-    <button class="btn">News</button>
-  </div>
-</nav>
+        </div>
+        <button className="btn">News</button>
+      </div>
+    </nav>
   );
 }
 
