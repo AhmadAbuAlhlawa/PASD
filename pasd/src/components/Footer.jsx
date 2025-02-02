ea55deb3fcb869fb945e17c6ee76adf73247241c
Safi100 <safinafi12@gmail.com>
2025-02-02 23:30:50 +0200

    update

--- pasd/src/components/Footer.jsx
+++ pasd/src/components/Footer.jsx
@@ -9,13 +9,12 @@ const Footer = () => {
     <div className='footer_container'>
         <div className="wrapper">
             <div className='footer_bottom'>
-                <p className='mb-0'>© {new Date().getFullYear()} All copyrights reserved for Al-Quds University, Palestine, Jerusalem.</p>
                 <div className='footer_links d-flex gap-3'>
                     <p className='pages_links mb-0'><a href="privacy-policy">Privacy Policy</a> | <a href="/about-us">About Us</a> | <a href="/support-us">Contact</a></p>
-                    <p className='social_links mb-0'>
-                        <a href='https://ps.linkedin.com/school/alquds-university/' target='_blank' rel='noopener noreferrer'><LinkedInIcon /></a>
-                        <a href='https://www.instagram.com/alquds.university' target='_blank' rel='noopener noreferrer'><InstagramIcon /></a>
-                        <a href='https://www.youtube.com/@AlQudsUniv' target='_blank' rel='noopener noreferrer'><YouTubeIcon /></a>
+                </div>
+                <div className='footer_bottom'>
+                <p className='mb-0'>© {new Date().getFullYear()} All copyrights reserved for Al-Quds University, Palestine, Jerusalem.</p>
+                <p className='social_links mb-0'>
                         <a href='https://www.alquds.edu/' target='_blank' className='logo'><img src="https://www.alquds.edu/wp-content/uploads/2022/02/black-logo.png" alt="AQU LOGO" /></a>
                     </p>
                 </div>
